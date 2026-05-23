c2ceb3ed61ec230c0dbf06455456b395d8be5124
Prambudialief <prambualif@gmail.com>
2026-05-23 09:27:37 +0700

    update terbaru

--- backend/index.js
+++ backend/index.js
@@ -39,9 +39,7 @@ const serviceAccount = JSON.parse(
   process.env.FIREBASE_SERVICE_ACCOUNT
 );
 
-initializeApp({
-  credential: cert(serviceAccount),
-});
+initializeApp({ credential: cert(serviceAccount) });
 const db = getFirestore();
 console.log("✅ Firestore terhubung!\n");
 
